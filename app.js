651678c46000492fa19c0782fdd6bd0bb82b309b
Jeyfferson <jeyffesilv@gmail.com>
2025-02-20 20:42:40 -0300

    melhorando as rotas com bodyParser

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const morgan= require('morgan');  
+const bodyParser = require('body-parser'); 
 
 const routerProdutos = require('./routes/produtos');
 const routerPedidos = require('./routes/pedidos');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false})); //Apenas dados simples
+app.use(bodyParser.json()); // json de entradas do body
 
 app.use('/produtos', routerProdutos);
 app.use('/pedidos', routerPedidos);
